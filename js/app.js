1518084eb382c57c22e548feba595a71f97f22b4
fahmid <dnetfahmidhasan@gmail.com>
2024-04-23 12:01:22 +0600

    making a overlay search bar and make it responsive

--- js/app.js
+++ js/app.js
@@ -45,7 +45,13 @@ $('#gallary-car').owlCarousel({
   }
 })
 
-
+function openSearch() {
+    document.getElementById("myOverlay").style.display = "block";
+  }
+  
+  function closeSearch() {
+    document.getElementById("myOverlay").style.display = "none";
+  }
 
 
 
